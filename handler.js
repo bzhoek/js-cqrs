1219da7708349e1f2ca82342779c7460f805a1ef
Bas van der Hoek <bas@hoek.com>
2018-10-18 10:00:49 +0200

    jest function calling based on data

--- handler.js
+++ handler.js
@@ -1,6 +1,16 @@
 export default class Handler {
 
   handle(data, aggregator) {
+    if (data === null || data === undefined) {
+      throw "Data parameter is not defined!";
+    }
+    if (data["command"] !== undefined) {
+      return this[`on${data["command"]}`].apply(null, [data, null])
+    }
     return false;
+  }
+
+  oncreate(data, aggregator) {
+    return true;
   }
 };
--- handler.test.js
+++ handler.test.js
@@ -1,8 +1,26 @@
 import Handler from './handler';
 
 describe('random numbers', () => {
+
   it('handles', () => {
     let subject = new Handler()
     expect(subject.handle).toBeDefined();
   })
+
+  it('handles as string', () => {
+    let subject = new Handler()
+    expect(subject["handle"]).toBeDefined();
+    expect(typeof subject['handle']).toBe('function');
+  })
+
+  it('calls as string', () => {
+    let subject = new Handler()
+    expect(subject["handle"].apply(null, [{}, null])).toBeFalsy();
+  })
+
+  it('calls from data', () => {
+    let subject = new Handler()
+    expect(subject["handle"].apply(subject, [{command: "create"}, null])).toBeTruthy();
+  })
+
 })
